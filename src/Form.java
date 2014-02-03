01979eaa771664363f2bc308156ec54239bfc625
Flamine <flamine@list.ru>
2014-02-03 23:30:13 +0400

    form

---
+++ src/Form.java
@@ -0,0 +1,14 @@
+/**
+ * Created by flamine on 03.02.14.
+ */
+
+import javax.swing.JFrame;
+
+public class Form extends JFrame {
+
+    public Form() {
+        super("Role"); //Заголовок окна
+        setBounds(400, 400, 500, 500);
+        setLocationRelativeTo(null);//выравниваем окно по центру
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { //эта функция может быть и в другом классе
+        Form app = new Form(); //Создаем экземпляр нашего приложения
+        app.setVisible(true); //С этого момента приложение запущено!
     }
 }
